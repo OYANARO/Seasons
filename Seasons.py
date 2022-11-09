cf0fbcb88f33ec38e938b0b9e9cd289f732956a6
OYANARO <zeus75181@gmail.com>
2022-11-09 21:03:39 +0300

    Initial commit

---
+++ Seasons.py
@@ -0,0 +1,11 @@
+x = int(input())
+if x >= 1 and x <=2:
+    print("зима")
+elif x >=3 and x<=5:
+    print("весна")
+elif x>=6 and x<=8:
+    print("лето")
+elif x>=9 and x<=11:
+    print("осень")
+elif x == 12:
+    print("зима")
