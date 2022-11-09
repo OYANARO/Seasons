1fedcfff8ee8697ca5ab75415384969a765a8c6a
OYANARO <97360451+OYANARO@users.noreply.github.com>
2022-11-09 21:07:50 +0300

    Update Seasons.py

--- Seasons.py
+++ Seasons.py
@@ -1,11 +1,11 @@
 x = int(input())
 if x >= 1 and x <=2:
-    print("зима")
+    print("Winter")
 elif x >=3 and x<=5:
-    print("весна")
+    print("Spring")
 elif x>=6 and x<=8:
-    print("лето")
+    print("Summer")
 elif x>=9 and x<=11:
     print("осень")
 elif x == 12:
-    print("зима")
+    print("Winter")
