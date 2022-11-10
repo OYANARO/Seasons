5e18e241e574a581cedee7d5c072f5f137a00925
Ffires <102007864+Ffires@users.noreply.github.com>
2022-11-10 22:13:03 +0300

    Update Seasons.py

--- Seasons.py
+++ Seasons.py
@@ -1,10 +1,10 @@
 x = int(input())
-if x in range[1, 2, 12]:
+if x in [1, 2, 12]:
     print("Winter")
-elif x in range[3, 4, 5]:
+elif x in list(3, 4, 5):
     print("Spring")
-elif x in range[6, 7, 8]:
+elif x in list(6, 7, 8):
     print("Summer")
-elif x in range[9, 10, 11]:
+elif x in list(9, 10, 11):
     print("осень")
     
